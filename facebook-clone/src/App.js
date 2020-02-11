3daa7f0020b0b7b1a6902fb44c9a20aae97c400e
Vincent Sanders <vincesanders@hotmail.com>
2020-02-11 17:59:10 -0600

    Fully styled navbar. Added MainContainer component.

--- facebook-clone/src/App.js
+++ facebook-clone/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import MainContainer from './components/MainContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainContainer />
       This is the App component
     </div>
   );
--- facebook-clone/src/components/Navbar.js
+++ facebook-clone/src/components/Navbar.js
@@ -1,9 +1,106 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
+import { faUserFriends, faBell, faQuestionCircle, faCaretDown, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Nav = styled.nav`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 42px;
+    width: 100%;
+    color: white;
+    background-color: #385898;
+    font-size: 14px;
+    font-weight: bold;
+    position: fixed;
+    .nav-contents-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 65%;
+        .nav-icon {
+            color: rgba(0,0,0,0.6);
+            font-size: 20px;
+            cursor: pointer;
+            &:hover {
+                color: rgba(0,0,0,0.9);
+            }
+        }
+        a {
+            color: white;
+            text-decoration: none;
+            padding: 4px 10px;
+            &:hover {
+                background-color: #1D3C78;
+            }
+        }
+    }
+`
+
+const SearchDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .facebook-icon {
+        cursor: pointer;
+        font-size: 30px;
+    }
+    form {
+        display: flex;
+        margin-left: 6px;
+        input {
+            height: 20px;
+            font-size: 14px;
+            width: 450px;
+            padding-left: 5px;
+        }
+        button {
+            float: left;
+            margin-left: -50px;
+            width: 50px;
+            font-size: 14px;
+            color: rgb(120,120,120);
+            background-color: rgba(210,210,210,0.3);
+            border: none;
+        }
+    }
+`
+
+const Profilelink = styled.a`
+    display: flex;
+    align-items: center;
+    img {
+        height: 26px;
+        width: 26px;
+        border-radius: 50%;
+        margin-right: 5px;
+    }
+`
 
 export default () => {
     return (
-        <nav>
-            This is the Navbar component.
-        </nav>
+        <Nav>
+            <div className='nav-contents-container'>
+                <SearchDiv>
+                    <Icon className='facebook-icon' icon={faFacebookSquare} />
+                    <form>
+                        <input placeholder='Search' />
+                        <button><Icon icon={faSearch} /></button>
+                    </form>
+                </SearchDiv>
+                <Profilelink href='#' ><img src={require('../assets/vince-profile.png')} />Vincent</Profilelink>
+                <a href='#'>Home</a>
+                <a href='#'>Find friends</a>
+                <a href='#'>Create</a>
+                <Icon className='nav-icon' icon={faUserFriends} />
+                <Icon className='nav-icon' icon={faFacebookMessenger} />
+                <Icon className='nav-icon' icon={faBell} />
+                <Icon className='nav-icon' icon={faQuestionCircle} />
+                <Icon className='nav-icon' icon={faCaretDown} />
+            </div>
+        </Nav>
     );
 }
